cac66768005bd547b10476603a2c31368c411397
Sangharsha Singh Kushwaha <shivamlali604@gmail.com>
2024-10-16 17:43:47 +0530

    #code pushed for add method of string calculator service

--- src/app/string-calculator.service.ts
+++ src/app/string-calculator.service.ts
@@ -8,6 +8,27 @@ export class StringCalculatorService {
   constructor() { }
 
   add(userInput: string): number{
-      return 0;
+    userInput = userInput?.trim() //trimming the user input 
+    if (!userInput) {
+      return 0; // if user enter empty string directly returing zero
+    }
+
+    if (userInput?.startsWith('//')) { // if user input have custom delimiters 
+      let endIndexOfDelimiter = userInput?.indexOf('\\');
+      userInput = userInput?.substring(endIndexOfDelimiter + 2); // Removing the custom delimiters declaration
+    }
+
+    let matchesNubers = userInput?.match(/-?\d+/g); // Using regex to find all numbers in the string including negative number
+    const numbersCollection = matchesNubers ? matchesNubers?.map(Number) : []; //Converting matches to numbers and return as numbersCollection array
+    let negatives: any = [];
+    let sum:number = 0;
+    numbersCollection.forEach((ele:any) =>{
+      ele < 0 ? negatives.push(ele) : sum +=ele > 1000 ? 0 : ele;
+    })
+
+    if (negatives.length > 0) {
+      throw new Error(`negatives not allowed: ${negatives.join(', ')}`);
+    }
+    return sum;
   }
 }
